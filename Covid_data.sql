0579716901ea0b6cf28291641dcea42a364ca0e8
MichaelGArmijo <107368251+MichaelGArmijo@users.noreply.github.com>
2022-06-26 18:08:38 -0600

    Update Covid_data.sql

--- Covid_data.sql
+++ Covid_data.sql
@@ -1,6 +1,8 @@
+--Michael G Armijo SQL requests using Microsoft SQL Server Management Studio
+
 --Hannah Ritchie, Edouard Mathieu, Lucas Rodés-Guirao, Cameron Appel, Charlie Giattino, 
-Esteban Ortiz-Ospina, Joe Hasell, Bobbie Macdonald, Diana Beltekian and Max Roser (2020) - "Coronavirus Pandemic (COVID-19)". 
-Published online at OurWorldInData.org. Retrieved from: 'https://ourworldindata.org/coronavirus' [Online Resource]
+--Esteban Ortiz-Ospina, Joe Hasell, Bobbie Macdonald, Diana Beltekian and Max Roser (2020) - "Coronavirus Pandemic (COVID-19)". 
+--Published online at OurWorldInData.org. Retrieved from: 'https://ourworldindata.org/coronavirus' [Online Resource]
 
 --Death Percentage
 
